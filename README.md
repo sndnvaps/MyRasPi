7ccbfb850d0c400eab9f24af7428b10a9e99788e
root <root@raspberrypi.lan>
2018-11-19 11:43:05 +0000

    remove all the unneed stuf
    Signed-off-by: root <root@raspberrypi.lan>

--- README.md
+++ README.md
@@ -1,41 +1,23 @@
-MyRasPi
-=======
+# Generate config.h file with ccmake 
+	apt-get install cmake-curses-gui 
 
-我的树莓派，最近会逐渐写一点运行在上面的python脚本。
- http://www.machtalk.net/device/dataview/df104baddce24fd0a5e976c90fc07df3 可以在这里看到结果。
+#generate Makefile for build 
+	cmake . 
 
-Add C language && golang support 
+#build 
+	make 
 
-Add DHT11 sensor support 
-Add DHT22 sensor support 
-
-Add machtalk.net support 
-
-add std::thread support (need c++11 support )
-
-add mysql support (dht22)
+#install
+	make install 
 
 
-# upload dht22 && rpi_cpu_temp to machtalk per minute 
+#Test if we insert the temp into database 
 
-	1. Copy cronfile to /home/pi/ folder 
-	2. Setup crontab 
-			$ crontab  /home/pi/cronfile 
-	3. Replace [machtalk/dht22/machtalk_dht22.cpp] line 69 && line 70 with you own info 
-	4. Build for you pi 
-			$ git clone https://github.com/sndnvaps/MyRasPi.git 
-			$ cd MyRasPi 
-			$ sudo su
-			$ cd machtalk
-			$ vim dht22/dht22.h # setup dht22 for you own gpio pin , i was set to gpio pin 7 
-			$ cmake . 
-			$ make install 
-	5. have fun ,you have success install machtalk_dht22 into you pi /usr/local/bin/ 
+	#select the lastest 10 record 
+	select * from pi_temps where id > (select max(id) from pi_temps)-10;
 
-
-#TODO 
-
-[] Add [iniparser](https://github.com/ndevilla/iniparser) support 
+	#select the lastest record 
+	select * from pi_temps where id = (select max(id) from pi_temps);
 
 
 
--- machtalk_dht22.cpp
+++ machtalk_dht22.cpp
@@ -15,7 +15,7 @@
 #include <functional>
 #include <atomic>
 #include <mutex>
-#include "config.h"
+//#include "config.h"
 #ifdef SUPPORT_MYSQL_DB
 #include "record_temp.hpp"
 #endif 
@@ -66,21 +66,21 @@ int main() {
     float temp;
     float hum;
     float rpi_cpu_temp;
-    string api_token = "95345cb2cbc945da978afb4441da6bcf"; //Replace with you api_token
-    string device_id  = "df104baddce24fd0a5e976c90fc07df3";  //Replace with you device_id 
+//    string api_token = "95345cb2cbc945da978afb4441da6bcf"; //Replace with you api_token
+//    string device_id  = "df104baddce24fd0a5e976c90fc07df3";  //Replace with you device_id 
     GetDht22_data(&temp, &hum);
     rpi_cpu_temp = Get_rpi_temp();
 #ifdef SUPPORT_MYSQL_DB
 	Insert(float2string(temp),float2string(hum),float2string(rpi_cpu_temp));
 #endif 
-     std::thread t1; //t1 is not a thread 
-     std::thread t2(Machtalk_Post,api_token,device_id,"1","1",temp,3);
-     std::thread t3(Machtalk_Post,api_token,device_id,"2","1",hum,3);
-     std::thread t4(Machtalk_Post,api_token,device_id,"3","1",rpi_cpu_temp,3);
-    
-     t2.join();
-     t3.join();   
-     t4.join();
+//     std::thread t1; //t1 is not a thread 
+//     std::thread t2(Machtalk_Post,api_token,device_id,"1","1",temp,3);
+//     std::thread t3(Machtalk_Post,api_token,device_id,"2","1",hum,3);
+//     std::thread t4(Machtalk_Post,api_token,device_id,"3","1",rpi_cpu_temp,3);
+//    
+//     t2.join();
+//     t3.join();   
+//     t4.join();
    return 0;
 }
 
