88c10c3c84cfd89e3c608b4a58dbcb8c4fab194f
sndnvaps <sndnvaps@gmail.com>
2019-11-11 15:29:37 +0800

    fix rpi-cpu-temp.go: pick up cpu temp from system

--- rpi-cpu-temp.go
+++ rpi-cpu-temp.go
@@ -10,10 +10,14 @@ func RpiCpuTemp() string {
 	temp_file := "/sys/class/thermal/thermal_zone0/temp"
 	file, _ := os.Open(temp_file)
 	defer file.Close()
-	buf := make([]byte, 15)
+	buf := make([]byte, 5)
 	n, _ := file.Read(buf)
-	//fmt.Println("buf = ", string(buf[:n]))
-	f, _ := strconv.ParseFloat(string(buf[:n]), 64)
-	rpi_temp := fmt.Sprintf("%2.2f", f/1000)
-	return rpi_temp
+	fmt.Println("buf = ", string(buf[:n]))
+	ori_temp := string(buf)
+        f, err := strconv.ParseFloat(ori_temp, 64)
+        if err != nil {
+           fmt.Printf("cann't convert rpi_cpu_temp str to float64 err_msg:[%s]",err.Error())
+        }
+        rpi_temp := fmt.Sprintf("%2.2f", f/1000)
+        return rpi_temp
 }
